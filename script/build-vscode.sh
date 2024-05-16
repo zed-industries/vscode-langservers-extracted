2212a1355006e4f3610e5e0f572008ce184b51f7
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2024-05-16 18:56:27 +0200

    Adjust the scripts for building zed fork

--- script/build-vscode.sh
+++ script/build-vscode.sh
@@ -8,7 +8,7 @@ mkdir -p $DIR/../dist
 
 # clone
 cd $DIR/../tmp
-git clone --depth=1 git@github.com:Microsoft/vscode vscode
+git clone --depth=1 --single-branch -b html-tags-as-workspace-edits git@github.com:zed-industries/vscode vscode
 
 # pull
 cd $DIR/../tmp/vscode
@@ -19,24 +19,25 @@ git pull --rebase
 # npm install
 cd $DIR/../tmp/vscode
 yarn
+cd ./extensions/html-language-features/server
 yarn compile
 
 # copy to dist
 cd $DIR/..
 
-mkdir -p ./dist/css-language-server
-cp -r ./tmp/vscode/extensions/css-language-features/server/out/* ./dist/css-language-server/
-npx babel ./dist/css-language-server --out-dir ./lib/css-language-server/
+#mkdir -p ./dist/css-language-server
+#cp -r ./tmp/vscode/extensions/css-language-features/server/out/* ./dist/css-language-server/
+#npx babel ./dist/css-language-server --out-dir ./lib/css-language-server/
 
 mkdir -p ./dist/html-language-server
 cp -r ./tmp/vscode/extensions/html-language-features/server/out/* ./dist/html-language-server/
 npx babel ./dist/html-language-server --out-dir ./lib/html-language-server/
 
-mkdir -p ./dist/json-language-server
-cp -r ./tmp/vscode/extensions/json-language-features/server/out/* ./dist/json-language-server/
-npx babel ./dist/json-language-server --out-dir ./lib/json-language-server/
+#mkdir -p ./dist/json-language-server
+#cp -r ./tmp/vscode/extensions/json-language-features/server/out/* ./dist/json-language-server/
+#npx babel ./dist/json-language-server --out-dir ./lib/json-language-server/
 
-mkdir -p ./dist/markdown-language-server
-cp -r ./tmp/vscode/extensions/markdown-language-features/server/out/* ./dist/markdown-language-server/
-npx babel ./dist/markdown-language-server --out-dir ./lib/markdown-language-server/
+#mkdir -p ./dist/markdown-language-server
+#cp -r ./tmp/vscode/extensions/markdown-language-features/server/out/* ./dist/markdown-language-server/
+#npx babel ./dist/markdown-language-server --out-dir ./lib/markdown-language-server/
 
